fe2fa3ea472ef78527a6cf1c36a72f9d22c1269f
Nguyen Tung <tungnguyen0408tnnm@gmail.com>
2024-12-23 10:43:57 +0700

    final

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $username = 'root';
 $password = '';
-$dbname = 'coffeeshop';
+$dbname = 'coffeshop';
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
